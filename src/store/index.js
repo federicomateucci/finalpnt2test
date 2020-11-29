f5e9a5d99a0bbee9495961c962246c76e3311ad5
federico mateucci <federicomateucci@gmail.com>
2020-11-29 19:34:20 -0300

    pnt2final

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,32 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
+  state() {
+    return{
+
+      msg:'VUEX DESDE CERO MANEJADO CON STORE',
+      input:'',
+      
+    }
   },
   mutations: {
+    chargeVar(state, inputFromHello){
+      
+      const replacements = {'A': 'U', 'E': 'O', 'O': 'E', 'U': 'A'};
+      let newinput = inputFromHello.toUpperCase();
+      state.input = newinput.replace(/A|E|O|U/g, char => replacements[char])
+    
+      
+    }
   },
   actions: {
+    chargeVarAction({commit}, inputFromHello){
+      commit('chargeVar',inputFromHello)
+    }
   },
   modules: {
+  },
+  getters:{
+
   }
 })
