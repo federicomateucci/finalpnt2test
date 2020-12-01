e2507a2ba25dcab021d604990821661d94edc22a
federico mateucci <federicomateucci@gmail.com>
2020-12-01 20:37:23 -0300

    Final examen PNT2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,83 @@
 <template>
+
+
+  <div class="jumbotron">
+    <input placeholder="COLOR" type="text" v-model="color">
+    <input placeholder="TAMANIO" type="text" v-model="tamanio">
+    <input placeholder="FONDO" type="text" v-model="fondo">
+    
+  </div>
   <div class="hello">
     <h1>{{ $store.state.msg }}</h1>
-    <input type="text" v-model="inputfromStore"/>
-    <p>{{inputfromStore}}</p>
-    <button @click="addToStore()">agregar a vuex store</button>
-   
-  
 
+    <input placeholder="TEXTO" :style="{fontSize:tamanio + 'px', color:color,backgroundColor:fondo}" type="text" v-model="inputfromStore" />
+    <p> desde vuex reverso {{ addToStore }}</p>
 
+    <p>Cantidad de palabras {{wordCount}}</p>
   </div>
-  <p>{{getInputFromStore()}}</p>
+ 
+
+<hr>
+
+  <table id="choiceawnsers">
+    <h3>Respuestas Choice</h3>
+  <br>
+  <tr>
+    <th>1.</th>
+    <th>2.</th>
+    <th>3.</th>
+    <th>4.</th>
+    <th>5.</th>
+  </tr>
+  <tr>
+    <td>B</td>
+    <td>C</td>
+    <td>B</td>
+    <td>D</td>
+    <td>D</td>
+  </tr>
+  </table>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  data(){
-    return{
-      inputfromStore:''
-    }
+  data() {
+    return {
+      inputfromStore: "",
+      color:'',
+      tamanio:'',
+      fondo:'',
+      // myStyle:{
+      //   backgroundColor:this.color,
+      //   color:this.color,
+      //   fontSize:this.tamanio
+        
+      // }
+    };
   },
-  methods:{
-    addToStore(){
-      this.$store.dispatch('chargeVarAction',this.inputfromStore)
-      this.inputfromStore='';
+  methods: {
+    getInputFromStore() {
+      return this.$store.state.input;
     },
-      getInputFromStore(){
-      return this.$store.state.input
-
-      
-    }
-    
-  
   },
-  computed:{
-  
-  }
-
+  computed: {
+    addToStore() {
+      this.$store.dispatch("chargeVarAction", this.inputfromStore);
+      return this.$store.state.input;
+    },
+    wordCount(){
+      const msgtoCount = this.inputfromStore.split(' ');
+      let wordstoCount = []
+      msgtoCount.forEach(word => {
+        if(word != ""){
+          wordstoCount.push(word)
+        }
+      })
+      return wordstoCount.length
+    }
  
+  },
 };
 </script>
 
@@ -57,4 +97,11 @@ li {
 a {
   color: #42b983;
 }
+
+#choiceawnsers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
 </style>
